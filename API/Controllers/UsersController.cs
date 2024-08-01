cf02bdd035713822db6b4fa698433d8350cf3bcc
lokeshumapathy <lolokesh94@gmail.com>
2024-08-01 18:28:12 +0530

    First Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    /*private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        this._context = context;
+    }
+    Usual way of inject a service in a controller, but in C# 12 we have concept of primary constructor.
+    */
+
+    [HttpGet]
+    //[Route("getusers")]
+ public ActionResult<IEnumerable<AppUser>> GetUsers()
+ {
+    var users = context.Users.ToList();
+    return Ok(users);
+ } 
+ [HttpGet("{id:int}")]
+ //[Route("getuser/{id}")]
+ public ActionResult<AppUser> GetUser(int id)
+ {
+    var user = context.Users.Find(id);
+    
+    if(user == null) return BadRequest();
+    return Ok(user);
+ }
+
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+ public int ID { get; set; }
+ public required string UserName { get; set; }
+}
